c35b69d370de610979ebc92c22226ca04325229b
Ben Dodd <ben-dodd@outlook.com>
2023-02-08 15:36:08 +0000

    Animal Example

---
+++ database/migrations/2023_02_08_144945_create_animals_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->double('weight', 8, 2);
+            $table->dateTime('date_of_birth')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('animals');
+    }
+};
